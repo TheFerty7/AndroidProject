b516708bbf4f71094bc8708122ffdc95da296007
Eric Lui <i-meric@hotmail.com>
2015-12-04 00:12:00 -0800

    Initial Commit

---
+++ COMP3717Project/app/src/main/java/com/example/comp3717project/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.comp3717project;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActionBarDrawerToggle dToggle;
+    private DrawerLayout dLayout;
+
+    private ListView mainListView;
+    private ArrayAdapter<String> listAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainListView = (ListView) findViewById(R.id.navList);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        dLayout = (DrawerLayout) findViewById(R.id.home_layout);
+        setUpDrawer();
+
+        String[] items = new String[]{"Recent", "Heroes", "Items"};
+
+        ArrayList<String> itemList = new ArrayList<String>();
+
+        itemList.addAll(Arrays.asList(items));
+
+        listAdapter = new ArrayAdapter<String>(this, R.layout.row, itemList);
+
+        mainListView.setAdapter(listAdapter);
+
+        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                if (position == 1) {
+                    Intent intent = new Intent(MainActivity.this, Heroes.class);
+                    startActivity(intent);
+                }
+                if (position == 2) {
+                    Intent intent = new Intent(MainActivity.this, Items.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+
+    }
+
+    private void setUpDrawer(){
+        dToggle = new ActionBarDrawerToggle(this, dLayout,
+                R.string.open, R.string.close) {
+        };
+        dToggle.setDrawerIndicatorEnabled(true);
+        dLayout.setDrawerListener(dToggle);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
