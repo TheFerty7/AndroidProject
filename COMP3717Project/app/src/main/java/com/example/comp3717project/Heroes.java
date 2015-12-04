9de46e94a853b23e902d8c0805cacb090e4b648b
Eric Lui <i-meric@hotmail.com>
2015-12-04 09:42:37 -0800

    items and heroes full list

--- COMP3717Project/app/src/main/java/com/example/comp3717project/Heroes.java
+++ COMP3717Project/app/src/main/java/com/example/comp3717project/Heroes.java
@@ -1,10 +1,12 @@
 package com.example.comp3717project;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,12 +15,25 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
 public class Heroes extends AppCompatActivity {
+
 
     private ActionBarDrawerToggle dToggle;
     private DrawerLayout dLayout;
@@ -26,13 +41,19 @@ public class Heroes extends AppCompatActivity {
     private ListView mainListView;
     private ArrayAdapter<String> listAdapter;
 
-    public String[] heroArray;
-    public String[] idArray;
+    public String[] heroesArray;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heroes);
+
+        String stringUrl = "https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=F7EB0CA4154233ABB155C2C98DEF9D02&language=en_us";
+
+
+        new DownloadWebpageTask().execute(stringUrl);
+
         mainListView = (ListView) findViewById(R.id.navList3);
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
@@ -69,15 +90,15 @@ public class Heroes extends AppCompatActivity {
             }
         });
 
-        initializeHeroList();
+
 
 
     }
 
-    private void initializeHeroList(){
+    private void initializeHeroList(String[] heroArray, String[] idArray){
         ListView heroes = (ListView) findViewById(R.id.listView3);
-        heroArray = new String[]{"Abbadon", "Alchemist"};
-        idArray = new String[]{"Hero id: 102", "Hero id: 73"};
+
+        heroesArray = heroArray;
 
         ArrayList<HashMap<String,String>> heroList = new ArrayList<HashMap<String,String>>();
         for(int i =0; i <heroArray.length; i++){
@@ -94,7 +115,7 @@ public class Heroes extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(Heroes.this, HeroDetail.class);
-                intent.putExtra("id", heroArray[position]);
+                intent.putExtra("id", heroesArray[position]);
                 startActivity(intent);
             }
         });
@@ -133,5 +154,81 @@ public class Heroes extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private String downloadUrl(String myurl) throws Exception {
+        InputStream in = null;
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+
+            // Start connection
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("HTTP-URL-CONNECTION", "The response is: " + response);
+            in = conn.getInputStream();
+
+            String result = readIt(in);
+            return result;
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
+
+    private String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        BufferedReader bufReader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        bufReader = new BufferedReader(reader);
+
+        String line;
+        String result = "";
+        while ((line = bufReader.readLine()) != null) {
+            result += line;
+        }
+
+        return result;
+    }
+
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadUrl(urls[0]);
+            } catch (Exception e) {
+                Log.e("Problem", e.getMessage());
+                return "error404";
+            }
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                if (result.equalsIgnoreCase("error404"))
+                    Toast.makeText(getApplicationContext(), "Unable to retrieve web page at the moment", Toast.LENGTH_LONG).show();
+
+                // Start json parser
+                JSONObject json = new JSONObject(result);
+                JSONArray matches = json.getJSONObject("result").getJSONArray("heroes");
+                String[] name = new String[matches.length()];
+                String[] id = new String[matches.length()];
+
+                for (int i = 0; i < matches.length(); i++) {
+                    name[i] = matches.getJSONObject(i).getString("localized_name");
+                    int temp =  matches.getJSONObject(i).getInt("id");
+                    id[i] = String.valueOf(temp);
+                }
+
+                initializeHeroList(name, id);
+            } catch (Exception ex) {
+                Log.d("Problem", ex.getMessage());
+            }
+        }
     }
 }
--- COMP3717Project/app/src/main/java/com/example/comp3717project/Items.java
+++ COMP3717Project/app/src/main/java/com/example/comp3717project/Items.java
@@ -1,10 +1,12 @@
 package com.example.comp3717project;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,7 +14,19 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -25,13 +39,18 @@ public class Items extends AppCompatActivity {
     private ListView mainListView;
     private ArrayAdapter<String> listAdapter;
 
-    public String[] itemArray;
-    public String[] idArray;
+    public String[] itemsArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
+
+        // Call to API and retrieve match data
+        String stringUrl = "https://api.steampowered.com/IEconDOTA2_570/GetGameItems/V001/?key=F7EB0CA4154233ABB155C2C98DEF9D02&language=en_us";
+
+        new DownloadWebpageTask().execute(stringUrl);
+
         mainListView = (ListView) findViewById(R.id.navList2);
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
@@ -68,15 +87,13 @@ public class Items extends AppCompatActivity {
             }
         });
 
-        initializeItemList();
-
 
     }
 
-    private void initializeItemList(){
+    private void initializeItemList(String[] itemArray, String[] idArray){
         ListView heroes = (ListView) findViewById(R.id.listView2);
-        itemArray = new String[]{"Iron Branch", "Gauntlets Of Strength"};
-        idArray = new String[]{"Item id: 1", "Item id: 73"};
+
+        itemsArray = itemArray;
 
         ArrayList<HashMap<String,String>> heroList = new ArrayList<HashMap<String,String>>();
         for(int i =0; i < itemArray.length; i++){
@@ -93,7 +110,7 @@ public class Items extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(Items.this, HeroDetail.class);
-                intent.putExtra("id", itemArray[position]);
+                intent.putExtra("id", itemsArray[position]);
                 startActivity(intent);
             }
         });
@@ -132,5 +149,83 @@ public class Items extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private String downloadUrl(String myurl) throws Exception {
+        InputStream in = null;
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+
+            // Start connection
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("HTTP-URL-CONNECTION", "The response is: " + response);
+            in = conn.getInputStream();
+
+            String result = readIt(in);
+            return result;
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
+
+    private String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        BufferedReader bufReader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        bufReader = new BufferedReader(reader);
+
+        String line;
+        String result = "";
+        while ((line = bufReader.readLine()) != null) {
+            result += line;
+        }
+
+        return result;
+    }
+
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadUrl(urls[0]);
+            } catch (Exception e) {
+                Log.e("Problem", e.getMessage());
+                return "error404";
+            }
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                if (result.equalsIgnoreCase("error404"))
+                    Toast.makeText(getApplicationContext(), "Unable to retrieve web page at the moment", Toast.LENGTH_LONG).show();
+
+                // Start json parser
+                JSONObject json = new JSONObject(result);
+                JSONArray matches = json.getJSONObject("result").getJSONArray("items");
+                String[] name = new String[matches.length()];
+                String[] id = new String[matches.length()];
+
+                for (int i = 0; i < matches.length(); i++) {
+                    name[i] = matches.getJSONObject(i).getString("localized_name");
+                    int temp =  matches.getJSONObject(i).getInt("cost");
+                    String t = String.valueOf(temp);
+                    id[i] = "Cost: " + t;
+                }
+
+                initializeItemList(name, id);
+            } catch (Exception ex) {
+                Log.d("Problem", ex.getMessage());
+            }
+        }
     }
 }
