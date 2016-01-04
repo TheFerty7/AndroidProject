1973fa5778541d4608db89ef6459a95fa5f03ce6
Duy Pham <duy.phamthe91@gmail.com>
2016-01-03 20:00:57 -0800

    Task finish: Loading match data to SQLite. Handling duplicate records from the API.
    Signed-off-by: Duy Pham (berong91) <duy.phamthe91@gmail.com>

--- Source/app/src/main/java/com/example/comp3717project/AppDatabaseHelper.java
+++ Source/app/src/main/java/com/example/comp3717project/AppDatabaseHelper.java
@@ -4,6 +4,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by Duy on 20/12/2015.
@@ -11,36 +12,68 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class AppDatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DB_NAME = "dota2_recent_game_analyzer_db";
-    public static final int DB_VERSION = 1;
-    public static final String HERO_TABLE = "HEROES";
+    public static final int DB_VERSION = 2;
+
+    public static final String MATCH_TABLE = "DOTA_2_MATCH";
+    public static final String MATCH_ID = "ID";
+    public static final String MATCH_SEQ_NUM = "SEQ_NUMBER";
+    public static final String MATCH_TIME = "MATCH_TIME";
+    public static final String MATCH_LOBBY = "LOBBY_TYPE";
+
+    public static final String HERO_TABLE = "DOTA_2_HEROES";
     public static final String HERO_ID = "ID";
     public static final String HERO_NAME = "NAME";
 
-    public static final String ITEM_TABLE = "ITEMS";
+    public static final String ITEM_TABLE = "DOTA_2_ITEMS";
     public static final String ITEM_ID = "ID";
     public static final String ITEM_NAME = "NAME";
 
     public AppDatabaseHelper(final Context context) {
         super(context, DB_NAME, null, DB_VERSION);
+        Log.d("Initial SQLite Instance", "Version " + DB_VERSION);
     }
 
     @Override
     public void onCreate(final SQLiteDatabase database) {
+        database.execSQL("CREATE TABLE " + MATCH_TABLE + " (" +
+                "ID_                INTEGER         PRIMARY KEY      AUTOINCREMENT, " +
+                MATCH_ID + "        TEXT            UNIQUE           ON CONFLICT IGNORE, " +
+                MATCH_SEQ_NUM + "   INTEGER         NOT NULL, " +
+                MATCH_TIME + "      TEXT            NOT NULL, " +
+                MATCH_LOBBY + "     TEXT            NOT NULL" +
+                ")");
+
         database.execSQL("CREATE TABLE " + HERO_TABLE + " (" +
                 "ID_                INTEGER        PRIMARY KEY      AUTOINCREMENT," +
-                HERO_ID + "      TEXT, " +
-                HERO_NAME + "    TEXT" +
-                ")");
+                HERO_ID + "         TEXT, " +
+                HERO_NAME + "       TEXT )");
+
         database.execSQL("CREATE TABLE " + ITEM_TABLE + " (" +
                 "ID_                INTEGER        PRIMARY KEY      AUTOINCREMENT," +
-                ITEM_ID + "      TEXT, " +
-                ITEM_NAME + "    TEXT" +
-                ")");
+                ITEM_ID + "         TEXT, " +
+                ITEM_NAME + "       TEXT )");
     }
 
+    public void insertMatch(final SQLiteDatabase database,
+                            final String id,
+                            final long seq_number,
+                            final String start_time,
+                            final String lobby_type) {
+        final ContentValues content;
+
+        content = new ContentValues();
+        content.put(MATCH_ID, id);
+        content.put(MATCH_SEQ_NUM, seq_number);
+        content.put(MATCH_TIME, start_time);
+        content.put(MATCH_LOBBY, lobby_type);
+
+        database.insert(MATCH_TABLE, null, content);
+    }
+
+
     public void insertHero(final SQLiteDatabase database,
-                              final String id,
-                              final String name) {
+                           final String id,
+                           final String name) {
         final ContentValues HeroValue;
 
         HeroValue = new ContentValues();
@@ -61,8 +94,14 @@ public class AppDatabaseHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(final SQLiteDatabase database,
+    public void onUpgrade(final SQLiteDatabase db,
                           final int oldVersion,
                           final int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + MATCH_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + HERO_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + ITEM_TABLE);
+        onCreate(db);
+
+        Log.d("Upgrade SQLite Instance", "Version " + newVersion);
     }
 }
--- Source/app/src/main/java/com/example/comp3717project/SplashActivity.java
+++ Source/app/src/main/java/com/example/comp3717project/SplashActivity.java
@@ -3,17 +3,34 @@ package com.example.comp3717project;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.net.ConnectivityManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class SplashActivity extends Activity {
 
-    private final int LOADING_TIME = 10;
-
-    TextView status;
+    public static String API_KEY = "12C01D8A57DFF90DB5C355DC37FDAB56";
+    public static int MATCH_REQUEST = 100;
+    public static int MIN_PLAYER = 10;
+    private final int LOADING_TIME = 3;
+    private SQLiteOpenHelper helper;
+    private TextView status;
+    private ConnectivityManager cm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,36 +41,105 @@ public class SplashActivity extends Activity {
         image.setImageResource(R.drawable.photo);
 
         Context context = this.getApplicationContext();
-        ConnectivityManager cm =
-                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
         status = (TextView) findViewById(R.id.txtStatus);
 
-        boolean isConnected;
+        helper = new AppDatabaseHelper(this);
 
-        while ((isConnected = (cm.getActiveNetworkInfo() == null &&
-                cm.getActiveNetworkInfo().isConnectedOrConnecting())) != true) {
-            status.setText("Please enable internet connection!");
-        }
+        String stringUrl = "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?" +
+                "key=" + API_KEY +
+                "&matches_requested=" + MATCH_REQUEST +
+                "&min_players=" + MIN_PLAYER;
 
-        new LoadingTask().execute();
+        new LoadingTask().execute(stringUrl);
     }
 
-    private class LoadingTask extends AsyncTask<Integer, String, Void> {
+    private String downloadUrl(String myurl) throws Exception {
+        InputStream in = null;
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+
+            // Start connection
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("HTTP-URL-CONNECTION", "The response is: " + response);
+            in = conn.getInputStream();
+
+            return readIt(in);
+        } catch (Exception ex) {
+            Log.e("Problem", ex.getMessage());
+            return "error404";
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
+
+    private String readIt(InputStream stream) throws IOException {
+        Reader reader = new InputStreamReader(stream, "UTF-8");
+        BufferedReader bufReader = new BufferedReader(reader);
+
+        String line;
+        String result = "";
+        while ((line = bufReader.readLine()) != null) {
+            result += line.replaceAll("\\s+", "");
+        }
+
+        return result;
+    }
+
+    private class LoadingTask extends AsyncTask<String, String, Void> {
         int i = LOADING_TIME;
 
         @Override
-        protected Void doInBackground(final Integer... params) {
+        protected Void doInBackground(final String... params) {
+            while (cm.getActiveNetworkInfo() == null
+                    || !cm.getActiveNetworkInfo().isAvailable()
+                    || !cm.getActiveNetworkInfo().isConnected()) {
+                publishProgress("Please enable internet connection!");
+            }
             new Thread() {
                 @Override
                 public void run() {
                     try {
+                        publishProgress("Calling to API");
+                        String result = downloadUrl(params[0]);
+
+                        if (result.equalsIgnoreCase("error404"))
+                            publishProgress("Unable to retrieve web page at the moment");
+
+                        publishProgress("Loading data");
+                        Thread.sleep(1500);
+                        // Start json parser
+                        JSONObject json = new JSONObject(result);
+                        JSONArray matches = json.getJSONObject("result").getJSONArray("matches");
+
+                        for (int i = 0; i < matches.length(); i++) {
+                            String id = matches.getJSONObject(i).getString("match_id");
+                            long seq_num = matches.getJSONObject(i).getLong("match_seq_num");
+                            String start_time = matches.getJSONObject(i).getString("start_time");
+                            String lobby_type = matches.getJSONObject(i).getString("lobby_type");
+
+                            Log.d("Match_", id + " seq_" + seq_num + " start_" + start_time + " lobby_" + lobby_type);
+
+                            ((AppDatabaseHelper) helper).insertMatch(helper.getWritableDatabase(), id, seq_num, start_time, lobby_type);
+                        }
+
                         while (i != 0) {
-                            publishProgress("System loading time: " + i);
+                            publishProgress("Application starts in " + i + " sec");
                             i--;
                             Thread.sleep(1000);
                         }
-                    } catch (Exception e) {
+                    } catch (Exception ex) {
+                        Log.e("Problem", ex.getMessage());
+                        publishProgress("Problem occurs. Please restart the app!");
                     }
                 }
             }.run();
@@ -69,9 +155,8 @@ public class SplashActivity extends Activity {
         protected void onPostExecute(Void aVoid) {
             Intent intent = new Intent(SplashActivity.this,
                     MainActivity.class);
-            startActivity(intent);
-            finish();
+//            startActivity(intent);
+//            finish();
         }
     }
-
 }
